418ba4e8e1100cafa1eb491d1bad4851d28f19be
Sergi Castarnado <scastarnadoo@uoc.edu>
2025-10-16 09:15:10 +0200

    Something was missing

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: '/porfolio',
 	server: {
 		port: 5173,
 		open: true,
